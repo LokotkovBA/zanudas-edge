70ad0c714010a664f0d581d91973fee29ec77a7d
LokotkovBA <woweuro2010@gmail.com>
2023-05-13 23:07:25 +0300

    disable trpc logger in prod

--- src/client/ClientProvider.tsx
+++ src/client/ClientProvider.tsx
@@ -57,7 +57,10 @@ export function ClientProvider(props: { children: React.ReactNode }) {
         clientAPI.createClient({
             links: [
                 loggerLink({
-                    enabled: () => true,
+                    enabled: (opts) =>
+                        process.env.NODE_ENV === "development" ||
+                        (opts.direction === "down" &&
+                            opts.result instanceof Error),
                 }),
                 httpBatchLink({
                     url: `${getBaseUrl()}/api/trpc`,
