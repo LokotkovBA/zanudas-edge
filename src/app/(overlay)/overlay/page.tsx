5a4a846dce20d537e9875cb187a2d96a46757132
LokotkovBA <woweuro2010@gmail.com>
2023-05-19 17:58:50 +0300

    add overlay route

--- src/app/(overlay)/overlay/page.tsx
+++ src/app/(overlay)/overlay/page.tsx
@@ -1,6 +1,15 @@
+import { serverAPI } from "~/server/api";
+
 export const runtime = "edge";
 export const preferredRegion = "arn1";
 
-export default function Overlay() {
-    return <>Overlay</>;
+export default async function Overlay() {
+    const data = await serverAPI.queue.getFiltered.fetch();
+    return (
+        <main className="bg-sky-400">
+            {data.map((entry) => (
+                <div key={entry.queue.id}>{entry.queue.songName}</div>
+            ))}
+        </main>
+    );
 }
