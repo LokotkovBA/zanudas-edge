a2e8bf5ea8818b62f2be58fe921201c86410a1c8
LokotkovBA <woweuro2010@gmail.com>
2023-05-27 23:28:54 +0300

    change overlay metadata

--- src/app/(overlay)/layout.tsx
+++ src/app/(overlay)/layout.tsx
@@ -1,5 +1,11 @@
 import { type PropsWithChildren } from "react";
 
+export const metadata = {
+    title: "Kalny overlay",
+    description: "Overlay for kalny streams",
+    themeColor: "#FFFFFF",
+};
+
 export default function OverlayLayout({ children }: PropsWithChildren) {
     return <body className="text-slate-50">{children}</body>;
 }
