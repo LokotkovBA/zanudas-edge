d858a909cf75d02508ca44384ea50aa388d85d53
LokotkovBA <woweuro2010@gmail.com>
2023-05-29 16:17:40 +0300

    add indication for signing in and out

--- src/components/client/SignInButton.tsx
+++ src/components/client/SignInButton.tsx
@@ -3,16 +3,24 @@ import clsx from "clsx";
 import { signIn } from "~/auth/client";
 import { buttonStyles } from "../styles/button";
 import TwitchIcon from "~/svg/TwitchIcon";
+import { toast } from "react-hot-toast";
 
 type SignInButtonProps = {
     className?: string;
 };
 
+async function onClick() {
+    toast.loading("Redirecting");
+    await signIn("twitch");
+    toast.dismiss();
+    toast.success("Success");
+}
+
 const SignInButton: React.FC<SignInButtonProps> = ({ className = "" }) => {
     return (
         <button
             className={clsx(className, buttonStyles, "flex items-center gap-1")}
-            onClick={() => signIn("twitch")}
+            onClick={onClick}
         >
             <TwitchIcon size="1.2rem" />
             Sign in
--- src/components/client/SignOutButton.tsx
+++ src/components/client/SignOutButton.tsx
@@ -2,16 +2,21 @@
 import clsx from "clsx";
 import { signOut } from "~/auth/client";
 import { buttonStyles } from "../styles/button";
+import { toast } from "react-hot-toast";
 type SignOutButtonProps = {
     className?: string;
 };
 
+async function onClick() {
+    toast.loading("Redirecting");
+    await signOut();
+    toast.dismiss();
+    toast.success("Success");
+}
+
 const SignOutButton: React.FC<SignOutButtonProps> = ({ className = "" }) => {
     return (
-        <button
-            className={clsx(className, buttonStyles)}
-            onClick={() => signOut()}
-        >
+        <button className={clsx(className, buttonStyles)} onClick={onClick}>
             Sign out
         </button>
     );
