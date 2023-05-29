e43b7cc544da1dff3c5bfb50f0a89fb2f7daef62
LokotkovBA <woweuro2010@gmail.com>
2023-05-29 13:41:18 +0300

    change intro route runtime to edge runtime

--- src/app/(stream)/intro/page.tsx
+++ src/app/(stream)/intro/page.tsx
@@ -1,6 +1,9 @@
 import { IntroOverlay } from "~/components/client/intro/IntroOverlay";
 import { serverAPI } from "~/server/api";
 
+export const runtime = "edge";
+export const preferredRegion = "arn1";
+
 export default async function Intro() {
     const introData = await serverAPI.intro.getAll.fetch();
     return <IntroOverlay introData={introData} />;
